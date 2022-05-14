7726d82f97acfed6a42f2e4be2d1bd71cfaf8c05
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2022-05-14 22:33:06 +0700

    todolist project restfulAPI with nodeJS

--- app.mjs
+++ app.mjs
@@ -5,10 +5,14 @@ const service = new TodolistService();
 const server = http.createServer((request, response) => {
 
     response.setHeader("Content-Type", "application/json")
-    if(request.method === "GET") {
+    if (request.method === "GET") {
         service.getTodoList(request, response);
-    } else if(request.method === "POST") {
-        service.createTodo(request, response)
+    } else if (request.method === "POST") {
+        service.createTodo(request, response);
+    } else if (request.method === "PUT") {
+        service.updateTodo(request, response);
+    } else if (request.method === "DELETE") {
+        service.deleteTodo(request, response);
     }
 });
 
--- todolist-service.mjs
+++ todolist-service.mjs
@@ -1,7 +1,6 @@
-
 export class TodolistService {
 
-    todolist = ["Ahmad", "Irfaan", "Hibatullah"];
+    todolist = [];
 
     getJSONTodoList() {
         return JSON.stringify({
@@ -16,12 +15,63 @@ export class TodolistService {
         })
     }
 
+    responseErrorNotFound() {
+        return JSON.stringify({
+            code: 404,
+            status: "OK",
+            data: "Not found data"
+        })
+    }
+
     getTodoList(request, response) {
         response.write(this.getJSONTodoList());
         response.end();
     }
 
     createTodo(request, response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            this.todolist.push(body.todo);
 
+            response.write(this.getJSONTodoList());
+            response.end();
+        })
+    }
+
+    updateTodo(request, response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            let index = body.id;
+            if (index >= this.todolist.length) {
+                response.write(this.responseErrorNotFound());
+                response.end();
+            } else {
+                if (this.todolist[index]) {
+                    this.todolist[index] = body.todo
+                }
+
+                response.write(this.getJSONTodoList());
+                response.end();
+            }
+        })
+    }
+
+    deleteTodo(request, response) {
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            let index = body.id;
+            if (index >= this.todolist.length) {
+                response.write(this.responseErrorNotFound());
+                response.end();
+            } else {
+                if (this.todolist[index]) {
+                    this.todolist.splice(index, 1);
+                }
+
+                response.write(this.getJSONTodoList());
+                response.end();
+            }
+
+        })
     }
 }
